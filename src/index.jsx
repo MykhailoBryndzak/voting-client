8230f0de3ceaed732c00e627e71759eb36d5147a
MykhailoBryndzak <mykhailo.bryndzak@gmail.com>
2016-10-05 09:01:17 +0300

    before ---------- Sending Actions To The Server Using Redux Middleware

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Voting from './components/Voting'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import reducer from './reducer'
+import App from './components/App';
+import {VotingContainer} from './components/Voting'
+import {ResultsContainer} from './components/Result';
+import io from 'socket.io-client'
+import {setState} from './action_creators'
+const store = createStore(reducer)
+store.dispatch({
+    type: 'SET_STATE',
+    state: {
+        vote: {
+            pair: ['Sunshine', '28 Days Later'],
+            tally: {Sunshine: 2}
+        }
+    }
+})
 
-const pair = ['Trainspotting', '28 Days Later']
+const socket = io(`${location.protocol}//${location.hostname}:8090`)
+socket.on('state', state =>
+    store.dispatch(setState(state))
+)
+
+const routes = <Route component={App}>
+    <Route path="/results" component={ResultsContainer} />
+    <Route path="/" component={VotingContainer} />
+</Route>;
 
 ReactDOM.render(
-    <Voting pair={pair} hasVoted='Trainspotting' winner="Trainspotting"/>,
+    <Provider store={store}>
+    <Router history={hashHistory}>{routes}</Router>
+    </Provider>,
     document.getElementById('app')
-)
+);
