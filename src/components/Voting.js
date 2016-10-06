df896593e8eb1db4690e471eddedf0d0dbeda6de
MykhailoBryndzak <mykhailo.bryndzak@gmail.com>
2016-10-06 14:43:44 +0300

    fin

--- src/components/Voting.js
+++ src/components/Voting.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import Winner from './Winner'
-import Vote from './Vote'
-import PureRenderMixin from 'react-addons-pure-render-mixin'
-import {connect} from 'react-redux'
+import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import {connect} from 'react-redux';
+import Winner from './Winner';
+import Vote from './Vote';
 import * as actionCreators from '../action_creators';
 
 export const Voting = React.createClass({
@@ -12,16 +12,19 @@ export const Voting = React.createClass({
             {this.props.winner ?
                 <Winner ref="winner" winner={this.props.winner} /> :
                 <Vote {...this.props} />}
-        </div>
+        </div>;
     }
-})
+});
 
 function mapStateToProps(state) {
     return {
         pair: state.getIn(['vote', 'pair']),
         hasVoted: state.get('hasVoted'),
         winner: state.get('winner')
-    }
+    };
 }
 
-export const VotingContainer = connect(mapStateToProps, actionCreators)(Voting)
+export const VotingContainer = connect(
+    mapStateToProps,
+    actionCreators
+)(Voting);
--- src/index.jsx
+++ src/index.jsx
@@ -1,29 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
-import {createStore} from 'redux'
-import {Provider} from 'react-redux'
-import reducer from './reducer'
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import io from 'socket.io-client';
+import reducer from './reducer';
+import {setState} from './action_creators';
+import remoteActionMiddleware from './remote_action_middleware';
 import App from './components/App';
-import {VotingContainer} from './components/Voting'
+import {VotingContainer} from './components/Voting';
 import {ResultsContainer} from './components/Result';
-import io from 'socket.io-client'
-import {setState} from './action_creators'
-const store = createStore(reducer)
-store.dispatch({
-    type: 'SET_STATE',
-    state: {
-        vote: {
-            pair: ['Sunshine', '28 Days Later'],
-            tally: {Sunshine: 2}
-        }
-    }
-})
 
-const socket = io(`${location.protocol}//${location.hostname}:8090`)
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
 socket.on('state', state =>
     store.dispatch(setState(state))
-)
+);
+
+const createStoreWithMiddleware = applyMiddleware(
+    remoteActionMiddleware(socket)
+)(createStore);
+const store = createStoreWithMiddleware(reducer);
 
 const routes = <Route component={App}>
     <Route path="/results" component={ResultsContainer} />
@@ -32,7 +28,7 @@ const routes = <Route component={App}>
 
 ReactDOM.render(
     <Provider store={store}>
-    <Router history={hashHistory}>{routes}</Router>
+        <Router history={hashHistory}>{routes}</Router>
     </Provider>,
     document.getElementById('app')
 );
